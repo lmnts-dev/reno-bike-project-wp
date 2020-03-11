e563362540d3c5a179e7e71f881592a4457a094d
agarric <alisha@laxaltandmciver.co>
2020-03-11 16:15:21 -0700

    :rocket: deploying: check multi dev

--- wordpress/wp-content/themes/cogs/assets/js/dist/vendor.js
+++ wordpress/wp-content/themes/cogs/assets/js/dist/vendor.js
@@ -1464,3 +1464,59 @@ function initLazyLoad() {
 initLazyLoad();
 // Vendor Javascript File
 "use strict";
+"use strict";
+
+/**
+ *
+ * Flickity.js settings
+ * @author Peter Laxalt
+ *
+ */
+function initSliders() {
+  // Re-init our block-sliders
+  var blockSliderElements = document.querySelectorAll(".block-slider-el");
+
+  if (blockSliderElements.length > 0) {
+    blockSliderElements.forEach(function (el) {
+      new Flickity(el, {
+        cellAlign: "left",
+        prevNextButtons: false,
+        fade: true,
+        wrapAround: true,
+        autoPlay: true
+      });
+    });
+  } // Re-init our collection-listing-sliders
+
+
+  var collectionListingsSliderElements = document.querySelectorAll(".collection-listings-slider-el");
+
+  if (collectionListingsSliderElements.length > 0) {
+    collectionListingsSliderElements.forEach(function (el) {
+      new Flickity(el, {
+        contain: true,
+        cellAlign: "left",
+        prevNextButtons: true,
+        pageDots: false,
+        freeScroll: true
+      });
+    });
+  } // Re-init our featured-product-sliders
+
+
+  var featuredProductsSlider = document.querySelectorAll(".featured-products-slider-el");
+
+  if (featuredProductsSlider.length > 0) {
+    featuredProductsSlider.forEach(function (el) {
+      new Flickity(el, {
+        cellAlign: "left",
+        prevNextButtons: true,
+        pageDots: false,
+        wrapAround: true,
+        autoPlay: true
+      });
+    });
+  }
+}
+
+initSliders();
