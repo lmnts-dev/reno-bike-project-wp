76cccee6047ed41d9dcc9a92811b730a143eb923
Peter Laxalt <pete@laxaltandmciver.co>
2020-03-11 00:42:57 -0400

    🎨 load barba locally

---
+++ pipeline/js/vendor/barbaSettings.js
@@ -0,0 +1,68 @@
+/**
+ *
+ * Barba.js settings
+ * @author Peter Laxalt
+ * @see https://barba.js.org/docs/userguide/markup/
+ *
+ */
+
+// basic default transition (with no rules and minimal hooks)
+barba.init({
+  transitions: [
+    {
+      leave({ current, next, trigger }) {
+        // do something with `current.container` for your leave transition
+        // then return a promise or use `this.async()`
+      },
+      beforeEnter({ current, next, trigger }) {
+        // Scroll to top of page
+        window.scrollTo(0, 0);
+
+        // Re-init our Lazy Loading
+        initLazyLoad();
+
+        // Re-init our bicycle wheel script.
+        initBikeWheel();
+
+        // Re-init our sliders
+        initSliders();
+      }
+    }
+  ]
+});
+
+// // dummy example to illustrate rules and hooks
+// barba.init({
+//   transitions: [{
+//     name: 'dummy-transition',
+
+//     // apply only when leaving `[data-barba-namespace="home"]`
+//     from: 'home',
+
+//     // apply only when transitioning to `[data-barba-namespace="products | contact"]`
+//     to: {
+//       namespace: [
+//         'products',
+//         'contact'
+//       ]
+//     },
+
+//     // apply only if clicked link contains `.cta`
+//     custom: ({ current, next, trigger })
+//       => trigger.classList && trigger.classList.contains('cta'),
+
+//     // do leave and enter concurrently
+//     sync: true,
+
+//     // available hooks…
+//     beforeOnce() {},
+//     once() {},
+//     afterOnce() {},
+//     beforeLeave() {},
+//     leave() {},
+//     afterLeave() {},
+//     beforeEnter() {},
+//     enter() {},
+//     afterEnter() {}
+//   }]
+// });
