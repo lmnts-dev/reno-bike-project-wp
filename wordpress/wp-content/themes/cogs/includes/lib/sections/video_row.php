68f3bea0709bdacc16a954cdfc82f6f0847a4efe
Peter Laxalt <pete@laxaltandmciver.co>
2020-03-12 22:40:13 -0400

    🎨 video row itself, starting overlay javascript

---
+++ wordpress/wp-content/themes/cogs/includes/lib/sections/video_row.php
@@ -0,0 +1,32 @@
+<?php
+
+/** 
+ * Video Row
+ * 
+ * @author Peter Laxalt
+ * @since 3/2020
+ * @stylesheet video-row.scss
+ * 
+ */
+
+/*************************************/
+/** Variables */
+/*************************************/
+
+if (get_row_layout() == 'video_row') {
+?>
+
+  <section class="video-row video-row-<?php echo $idx ?>">
+    <div class="video-row-inner">
+
+      <span class="video-toggle">
+        <span></span>
+      </span>
+      <iframe src="https://player.vimeo.com/video/76979871?background=1&autoplay=1&loop=1&byline=0&title=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      <img data-src="https://source.unsplash.com/1600x900/?cyan" class="cover lazy" alt="Alt Text" />
+
+    </div>
+  </section>
+
+
+<?php } ?>
