5b1ae6c08a557f550f7035daf2e102de776fba50
Alisha <alishagarric@hotmail.com>
2020-03-23 16:27:01 -0700

    :construction: progress: hooking up shopify product page

--- wordpress/wp-content/themes/cogs/assets/js/dist/app.js
+++ wordpress/wp-content/themes/cogs/assets/js/dist/app.js
@@ -56,6 +56,20 @@ function initNewsletter() {
 initNewsletter();
 "use strict";
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("mobile-nav-icon").addEventListener('click', function () {
+    document.getElementById("overlay-nav").classList.add("visible");
+    document.getElementsByTagName("header")[0].classList.add("hidden");
+    document.getElementsByTagName("body")[0].classList.add("overlay-lock");
+  });
+  document.getElementById("overlay-exit").addEventListener('click', function () {
+    document.getElementById("overlay-nav").classList.remove("visible");
+    document.getElementsByTagName("header")[0].classList.remove("hidden");
+    document.getElementsByTagName("body")[0].classList.remove("overlay-lock");
+  });
+});
+"use strict";
+
 /*
  ** @author: Peter Laxalt
  ** @description: Super simple script for the Bike Wheel SVG to
@@ -182,17 +196,3 @@ function initVideoOverlay() {
 }
 
 initVideoOverlay();
-"use strict";
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("mobile-nav-icon").addEventListener('click', function () {
-    document.getElementById("overlay-nav").classList.add("visible");
-    document.getElementsByTagName("header")[0].classList.add("hidden");
-    document.getElementsByTagName("body")[0].classList.add("overlay-lock");
-  });
-  document.getElementById("overlay-exit").addEventListener('click', function () {
-    document.getElementById("overlay-nav").classList.remove("visible");
-    document.getElementsByTagName("header")[0].classList.remove("hidden");
-    document.getElementsByTagName("body")[0].classList.remove("overlay-lock");
-  });
-});
