6daf47ffaae91a5c245d4f8a694c2bb6324acfc0
Alisha <alishagarric@hotmail.com>
2020-04-16 10:40:28 -0700

    :rocket: deploying: altering existing components like block row and wysiwyg section to be able to create all versions in the prototype

--- wordpress/wp-content/themes/cogs/includes/lib/sections/wysiwyg_content_section.php
+++ wordpress/wp-content/themes/cogs/includes/lib/sections/wysiwyg_content_section.php
@@ -18,6 +18,10 @@ if (get_row_layout() == 'wysiwyg_content_section' || $rowLayout == 'wysiwyg_cont
 
   <section class="wysiwyg-content-section <?php echo get_sub_field('layout'); ?> wysiwyg-content-section-<?php echo $idx ?>">
     <div class="wysiwyg-content-section-inner article-body">
+        <?php if ( get_sub_field('lead_content_with_a_squiggle') ) { ?>
+            <?php $squiggle['color'] = get_sub_field('squiggle_color') ?>
+            <?php require ( get_template_directory() . "/assets/images/squiggle-vertical.php");  ?>
+        <?php } ?>
         <?php echo get_sub_field('content') ?>
     </div>
   </section>
